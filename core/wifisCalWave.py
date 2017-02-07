db8f6caa51db52b11daa2058a4ce86f1eac0b645
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-02-07 11:26:18 -0500

    Modified wifisCalWave to include header from input data and better deal with previous calibration solution.

--- core/wifisCalWave.py
+++ core/wifisCalWave.py
@@ -36,102 +36,120 @@ savename = '/processed/'+filename
 
 t0 = time.time()
 
-#Read in data
-t1 = time.time()
-out = wifisIO.readFromFolder(foldername)
-print("time to read all files took", time.time()-t1, " seconds")
-
-data = out[1]
-inttime = np.array(out[0])
-nFrames = inttime.shape[0]
-nx = data.shape[1]
-ny = data.shape[0]
-#******************************************************************************
-
-#Correct data for reference pixels
-ta = time.time()
-print("Subtracting reference pixel channel bias")
-#refCor.channelCL(data, nFrames, 32)
-print("Subtracting reference pixel row bias")
-#refCor.row(data, nFrames, 4)
-print("time to apply reference pixel corrections ", time.time()-ta, " seconds")
-#******************************************************************************
-
-#find if any pixels are saturated to avoid use in future calculations
-#*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-satFile = glob.glob('processed/*satCounts.fits')[0]
-satCounts = wifisIO.readImgFromFile(satFile)
-satFrame = satInfo.getSatFrameCL(data, satCounts,32)
-#******************************************************************************
-
-#apply non-linearity correction
-ta = time.time()
-print("Correcting for non-linearity")
-
-#find NL coefficient file
-#*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-nlFile = glob.glob('processed/*NLCoeff.fits')[0]
-
-nlCoeff = wifisIO.readImgFromFile(nlFile)
-NLCor.applyNLCorCL(data, nlCoeff, 32)
-print("time to apply non-linearity corrections ", time.time()-ta, " seconds")
-
-#******************************************************************************
-
-#Combine data into single image
-fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)
-
-data = 0
-
-#******************************************************************************
-#Correct for dark current
-#Identify appropriate dark image for subtraction
-darkName = 'processed/master_dark_I'+str(inttime[-1])+'.fits'
-if (os.path.exists(darkName)):
-    darkImg = wifisIO.readImgFromFile(darkName)
-    flxImg = flxImg - darkImg
-else:
-    cont = wifisIO.userInput('No corresponding master dark image could be found, do you want to proceed without dark subtraction (y/n)?')
-    if (cont.lower() == 'n'):
-        exit()
-
-#******************************************************************************    
-#write image to a file
-# *** STILL TO DO - SAVE SATURATION INFO AS ANOTHER HDU OR DIFFERENT FILE ***
-
 if(os.path.exists(savename+'_waveCal.fits')):
-    cont = wifisIO.userInput('Processed waveCal file already exists for ' +foldername+', do you want to replace (y/n)?')
+    cont = wifisIO.userInput('Processed waveCal file already exists for ' +foldername+', do you want to continue processing (y/n)?')
     if (cont.lower() == 'y'):
-        wifisIO.writeFits(fluxImg, savename+'_waveCal.fits', hdr=['INTTIME',inttime[-1],'Total integration time'])
+        contProc = True
+    else:
+        contProc = False
 else:
-    wifisIO.writeFits(fluxImg, savename+'_waveCal.fits', hdr=['INTTIME',inttime[-1],'Total integration time'])
+    contProc = True
+    
+if (contProc):
+    
+    #Read in data
+    t1 = time.time()
+    data, inttime, hdr = wifisIO.readFromFolder(foldername)
+    print("time to read all files took", time.time()-t1, " seconds")
 
-#******************************************************************************
-#Determine dispersion solution
+    nFrames = inttime.shape[0]
+    nx = data.shape[1]
+    ny = data.shape[0]
+    #******************************************************************************
 
-#read in template
-template = wifisIO.readImgFromFile(templatename+'_waveCal.fits')
+    #Correct data for reference pixels
+    ta = time.time()
+    print("Subtracting reference pixel channel bias")
+    #refCor.channelCL(data, nFrames, 32)
+    print("Subtracting reference pixel row bias")
+    #refCor.row(data, nFrames, 4)
+    print("time to apply reference pixel corrections ", time.time()-ta, " seconds")
+    #******************************************************************************
 
-#provide line atlas file
-atlasname = 'external_data/best_lines2.dat'
+    #find if any pixels are saturated to avoid use in future calculations
+    #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
+    satFile = glob.glob('processed/*satCounts.fits')[0]
+    satCounts = wifisIO.readImgFromFile(satFile)
+    satFrame = satInfo.getSatFrameCL(data, satCounts,32)
+    #******************************************************************************
 
-#read in dispersion solution corresponding to template
-prevSol = wifisIO.readTable(templatename+'_waveSol.dat')
+    #apply non-linearity correction
+    ta = time.time()
+    print("Correcting for non-linearity")
 
-#check if solution already exists.
-if(os.path.exists(savename+'_waveSol.dat')):
-    cont = wifisIO.userInput('Dispersion solution already exists for ' +foldername+', do you want to continue and replace (y/n)?')
-    if (cont.lower() == ''):
-        exit()
+    #find NL coefficient file
+    #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
+    nlFile = glob.glob('processed/*NLCoeff.fits')[0]
 
-#data image, template, atlas file, max fitting order, list of prev. solutions, dispersion axis direction, window range fro line fitting, maximum allowable cross-correlation pixel offset
-result = waveSol.getWaveSol(fluxImg, template, atlasname, 1, prevSol, dispAxis=1, winRng=7, mxCcor=30)
+    nlCoeff = wifisIO.readImgFromFile(nlFile)
+    NLCor.applyNLCorCL(data, nlCoeff, 32)
+    print("time to apply non-linearity corrections ", time.time()-ta, " seconds")
 
-#Save dispersion solution
-dispSol = []
-for sol in result:
-    dispSol.append(sol[0])
+    #******************************************************************************
 
-wifisIO.writeTable(savename+'_waveSol.dat', dispSol)
+    #Combine data into single image
+    fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)
 
+    data = 0
+
+    #******************************************************************************
+    #Correct for dark current
+    #Identify appropriate dark image for subtraction
+    darkName = 'processed/master_dark_I'+str(inttime[-1])+'.fits'
+    if (os.path.exists(darkName)):
+        darkImg = wifisIO.readImgFromFile(darkName)
+        flxImg = flxImg - darkImg
+    else:
+        cont = wifisIO.userInput('No corresponding master dark image could be found, do you want to proceed without dark subtraction (y/n)?')
+        if (cont.lower() == 'n'):
+            exit()
+
+    #******************************************************************************    
+    #write image to a file
+    # *** STILL TO DO - SAVE SATURATION INFO AS ANOTHER HDU OR DIFFERENT FILE ***
+
+    if(os.path.exists(savename+'_waveCal.fits')):
+        cont = wifisIO.userInput('Processed waveCal file already exists for ' +foldername+', do you want to replace (y/n)?')
+        if (cont.lower() == 'y'):
+            contWrite = True
+        else:
+            contWrite = False
+    else:
+        contWrite = True
+        
+
+    if (contWrite):
+        #add or modify fits header here
+        wifisIO.writeFits(fluxImg, savename+'_waveCal.fits', hdr=hdr,'Total integration time'])
+   
+    #******************************************************************************
+    #Determine dispersion solution
+
+    #read in template
+    template = wifisIO.readImgFromFile(templatename+'_waveCal.fits')
+
+    #provide line atlas file
+    atlasname = 'external_data/best_lines2.dat'
+
+    #read in dispersion solution corresponding to template
+    prevSol = wifisIO.readTable(templatename+'_waveSol.dat')
+
+    #check if solution already exists.
+    if(os.path.exists(savename+'_waveSol.dat')):
+        cont = wifisIO.userInput('Dispersion solution already exists for ' +foldername+', do you want to continue and replace (y/n)?')
+        if (cont.lower() == 'n'):
+            exit()
+
+    #data image, template, atlas file, max fitting order, list of prev. solutions, dispersion axis direction, window range fro line fitting, maximum allowable cross-correlation pixel offset
+    result = waveSol.getWaveSol(fluxImg, template, atlasname, 1, prevSol, dispAxis=1, winRng=7, mxCcor=30)
+
+    #Save dispersion solution
+    dispSol = []
+    for sol in result:
+        dispSol.append(sol[0])
+
+    wifisIO.writeTable(savename+'_waveSol.dat', dispSol)
+
+t1 = time.time()
+print ("Total time to run entire script: ",t1-t0)
 
