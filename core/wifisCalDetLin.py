e959e53711af251dcc7f164d0176e6e2c1945767
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-04-07 09:41:32 -0400

    Modified script to address some bugs. Still needs updating.

--- core/wifisCalDetLin.py
+++ core/wifisCalDetLin.py
@@ -23,7 +23,7 @@ import wifisRefCor as refCor
 import os
 import wifisIO 
 
-os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1' # Used to show compile errors for debugging, can be removed
+os.environ['PYOPENCL_COMPILER_OUTPUT'] = '0' # Used to show compile errors for debugging, can be removed
 os.environ['PYOPENCL_CTX'] = ':1' # Used to specify which OpenCL device to target, should uncomment and set to preferred device to avoid interactively selecting each time
 
 t0 = time.time()
@@ -32,18 +32,19 @@ t0 = time.time()
 #*************************** Required input **********************************
 
 #set folder name
-foldername = 'test1'
+#filename = 'test1'
+filename = 'wifis_h2rg_singleend_100khz_12db.24.1.1.fits'
 
 #*****************************************************************************
 
-savename = 'processed/'+foldername
+savename = 'processed/'+filename
 
 #create processed directory
 wifisIO.createDir('processed')
 
 #check if processing needs to be done
 if(os.path.exists(savename+'_NLCoeff.fits') and os.path.exists(savename+'_satCounts.fits')):
-    cont = wifisIO.userInput('Non-linearity processed files already exists for ' +foldername+', do you want to continue processing (y/n)?')
+    cont = wifisIO.userInput('Non-linearity processed files already exists for ' +filename+', do you want to continue processing (y/n)?')
     if (cont.lower() == 'n'):
         contProc = False
     else:
@@ -56,7 +57,9 @@ if (contProc):
     #******************************************************************************
     #Read in data
     t1 = time.time()
-    data, inttime, hdr = wifisIO.readImgsFromFolder(foldername)
+    #data, inttime, hdr = wifisIO.readImgsFromFolder(filename)
+    data, inttime, hdr = wifisIO.readImgsFromFile(filename)
+
     print("time to read all files took", time.time()-t1, " seconds")
     
     nFrames = inttime.shape[0]
@@ -76,42 +79,196 @@ if (contProc):
 
     #******************************************************************************
     #get saturation information for each pixel
-    ta = time.time()
-    satCounts = satInfo.getSatCountsCL(data,0.95, 32)
-    satFrame = satInfo.getSatFrameCL(data,satCounts,32)
-    print ("saturation code took ", time.time()-ta, " seconds")
     
-    #write saturation info to array
-    #check if file should be saved
-        
     if(os.path.exists(savename+'_satCounts.fits')):
-        cont = wifisIO.userInput('satCounts file already exists for ' +foldername+', do you want to replace (y/n)?')
-        if (cont.lower() == 'y'):
-            wifisIO.writeFits(satCounts, savename+'_satCounts.fits') #ADD HEADER INFO?
-        else:
-            wifisIO.writeFits(satCounts, savename+'_satCounts.fits') #ADD HEADER INFO?
+        cont = wifisIO.userInput('satCounts file already exists for ' +filename+', do you want to replace (y/n)?')
+    else:
+        cont = 'y'
+
+    if (cont.lower() == 'y'):
+        print('Determining saturation info')
+        ta = time.time()
+        satCounts = satInfo.getSatCountsCL(data,0.95, 32)
+        satFrame = satInfo.getSatFrameCL(data,satCounts,32)
+        print ("saturation code took ", time.time()-ta, " seconds")
+
+        #write saturation info to file
+        wifisIO.writeFits(satCounts, savename+'_satCounts.fits') #ADD HEADER INFO?
+    else:
+        print('Reading saturation info from file')
+        satCounts = wifisIO.readImgFromFile(savename+'_satCounts.fits')[0]
+        print('Determining first saturated frame')
+        satFrame = satInfo.getSatFrameCL(data,satCounts,32)
      
     #******************************************************************************
-    
     #******************************************************************************
+
     # Get the non-linearity correction coefficients
-    ta = time.time()
-    nlCoeff = NLCor.getNLCorCL(data,satFrame,32)
-    print ("non-linearity code took", time.time()-ta, " seconds")
-    
+        
     #write NL Coefficients to a FITS image
     if(os.path.exists(savename+'_NLCoeff.fits')):
-        cont = wifisIO.userInput('NLCoeff file already exists for ' +foldername+', do you want to replace (y/n)?')
-        if (cont.lower() == 'y'):
-            wifisIO.writeFits(nlCoeff, savename+'_NLCoeff.fits')
+        cont = wifisIO.userInput('NLCoeff file already exists for ' +filename+', do you want to replace (y/n)?')
     else:
+        cont = 'y'
+
+    if (cont.lower() == 'y'):
+        print('Determining non-linearity corrections')
+        ta = time.time()
+        nlCoeff = NLCor.getNLCorCL(data,satFrame,32)
+        print ("non-linearity code took", time.time()-ta, " seconds")
+
+        #save file
         wifisIO.writeFits(nlCoeff, savename+'_NLCoeff.fits')
+    else:
+        print('Reading non-linearity coefficient file')
+        nlCoeff = wifisIO.readImgFromFile(savename+'_NLCoeff.fits')[0]
         
     #******************************************************************************
 
-    data =0 #clean up data cube to reduce memory usage
+    print('Done processing and determining non-linearity coefficients')
+    data = 0 #clean up data cube to reduce memory usage
+
 else:
-    print('Nothing to do, exiting')
+    print('No processing necessary')
+    
+#check if analysis of NL coefficients needs to be done
+if(os.path.exists('processed/bad_pixel_mask.fits')):
+    cont = wifisIO.userInput('bad pixel mask already exists, do you want to update, replace, skip? (update/replace/skip)')
+    
+    if (cont.lower() == 'update' or cont.lower() == 'replace'):
+        contAnalysis = True
+    else:
+        contAnalysis = False
+else:
+    contAnalysis = True
+    cont = 'replace'
+        
+if (contAnalysis):
+    print('Determining bad pixels from non-linearity coefficients')
+
+    if (~contProc):
+        #read in nlCoeff instead
+        print('Reading non-linearity coefficient file')
+        nlCoeff = wifisIO.readImgFromFile(savename+'_NLCoeff.fits')[0]
+    
+    if (cont == 'update'):
+        BPM = wifisIO.readImgFromFile('processed/bad_pixel_mask.fits')[0]
+    else:
+        BPM = np.zeros(nlCoeff[:,:,0].shape, dtype='int8')
+
+    print('Analyzing non-linearity coefficients and determining outliers')
+    #NLCoeff 0
+    n = nlCoeff[:,:,0]
+    
+    for i in range(5):
+        med = np.median(n)
+        std = np.std(n)
+        whr = np.where(np.abs(n-med) < 5.*std)
+        n = n[whr]
+
+    plt.close('all')
+    plt.hist(nlCoeff[:,:,0].flatten(), range=[med-std, med+std],bins=100)
+    hist = np.histogram(nlCoeff[:,:,0].flatten(), range=[med-1.5*std, med+1.5*std], bins=100)
+   
+    std = np.std(n)
+    med = np.median(n)
+    
+    plt.plot([med-5.*std,med-5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.plot([med+5.*std,med+5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.xlabel('value')
+    plt.ylabel('count')
+    plt.title('NL correction coefficient 0')
+    plt.savefig('nlcoeff_0_hist.eps', bbox_inches='tight')
+    #plt.show()
+    plt.close('all')
+    
+    whr = np.where(np.abs(nlCoeff[:,:,0]-med) > 5.*std)
+    BPM[whr] = 1
+    
+    #NLCoeff 1
+    n = nlCoeff[:,:,1]
+    
+    for i in range(6):
+        med = np.median(n)
+        std = np.std(n-med)
+        whr = np.where(np.abs(n-med) < 7.*std)
+        n = n[whr]
+
+    plt.hist(nlCoeff[:,:,1].flatten(), range=[med-std, med+std],bins=100)
+    hist = np.histogram(nlCoeff[:,:,1].flatten(), range=[med-std, med+std], bins=100)
+
+    med = np.median(n)
+    std = np.std(n)
+   
+    plt.plot([med-5.*std,med-5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.plot([med+5.*std,med+5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+
+    plt.xlabel('value')
+    plt.ylabel('count')
+    plt.title('NL correction coefficient 1')
+    plt.savefig('nlcoeff_1_hist.eps')
+    #plt.show()
+    plt.close('all')
+
+    whr = np.where(np.abs(nlCoeff[:,:,1]-med) > 5.*std)
+    #BPM[whr] = 1
+
+    #NLCoeff 2
+    n = nlCoeff[:,:,2]
+    
+    for i in range(8):
+        med = np.median(n)
+        std = np.std(n)
+        whr = np.where(np.abs(n-med) < 7.*std)
+        n = n[whr]
+        
+    plt.hist(nlCoeff[:,:,2].flatten(), range=[med-std, med+std],bins=100)
+    hist = np.histogram(nlCoeff[:,:,2].flatten(), range=[med-std, med+std], bins=100)
+
+    med = np.median(n)
+    std = np.std(n)
+    
+    plt.plot([med-5.*std,med-5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.plot([med+5.*std,med+5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.xlabel('value')
+    plt.ylabel('count')
+    plt.title('NL correction coefficient 2')
+    plt.savefig('nlcoeff_2_hist.eps')
+    #plt.show()
+    plt.close('all')
+
+    whr = np.where(np.abs(nlCoeff[:,:,2]-med) > 5.*std)
+    #BPM[whr] = 1
+
+    #NLCoeff 3
+    n = nlCoeff[:,:,3]
+    
+    for i in range(9):
+        med = np.median(n)
+        std = np.std(n)
+        whr = np.where(np.abs(n-med) < 5.*std)
+        n = n[whr]
+            
+    plt.hist(nlCoeff[:,:,3].flatten(), range=[med-std, med+std],bins=100)
+    hist = np.histogram(nlCoeff[:,:,3].flatten(), range=[med-std, med+std], bins=100)
+
+    med = np.median(n)
+    std = np.std(n)
+    
+    plt.plot([med-5.*std,med-5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.plot([med+5.*std,med+5.*std], [np.min(hist[0]),np.max(hist[0])], 'r--')
+    plt.xlabel('value')
+    plt.ylabel('count')
+    plt.title('NL correction coefficient 3')
+    plt.savefig('nlcoeff_3_hist.eps')
+    #plt.show()
+    plt.close('all')
+
+    whr = np.where(np.abs(nlCoeff[:,:,3]-med) > 5.*std)
+    #BPM[whr] = 1
+
+    print('Saving bad pixel mask')
+    wifisIO.writeFits(BPM.astype('int'),'processed/bad_pixel_mask.fits')
 
 t1 = time.time()
 print ("Total time to run entire script: ",t1-t0)
