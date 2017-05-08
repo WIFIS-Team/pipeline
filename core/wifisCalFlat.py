ecca5634bf59da8b9a4e40912285cb43e8aa0ba8
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-05-08 13:59:18 -0400

    Modified script into more workable state.

--- core/wifisCalFlat.py
+++ core/wifisCalFlat.py
@@ -5,6 +5,7 @@ Calibrate flat field images
 Produces:
 - master flat image
 - slitlet traces
+- ??
 
 """
 
@@ -17,8 +18,10 @@ import wifisRefCor as refCor
 import os
 import wifisIO 
 import wifisCombineData as combData
-import glob
 import wifisSlices as slices
+import wifisUncertainties
+import wifisBadPixels as badPixels
+import astropy.io.fits as fits
 
 os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1' # Used to show compile errors for debugging, can be removed
 os.environ['PYOPENCL_CTX'] = ':1' # Used to specify which OpenCL device to target. Should be uncommented and pointed to correct device to avoid future interactive requests
@@ -26,9 +29,23 @@ os.environ['PYOPENCL_CTX'] = ':1' # Used to specify which OpenCL device to targe
 t0 = time.time()
 
 #*****************************************************************************
-#************************** Required input ***********************************
+#******************************* Required input ******************************
 fileList = 'list' 
+nlFile = 'processed/master_detLin_NLCoeff.fits'        
+satFile = 'processed/master_detLin_satCounts.fits'
+bpmFile = 'processed/bad_pixel_mask.fits'
 #*****************************************************************************
+
+#first check if required input exists
+if not (os.path.exists(nlFile) and os.path.exists(satFile)):
+    if not (os.path.exists(satFile)):
+        print ('*** ERROR: Cannot continue, file ' + satFile + ' does not exist. Please process the a detector linearity calibration sequence or provide the necessary file ***')
+    if not (os.path.exists(nlFile)):
+        print ('*** ERROR: Cannot continue, file ' + nlFile + ' does not exist. Please process the a detector linearity calibration sequence or provide the necessary file ***')
+    raise SystemExit('*** Missing required calibration files, exiting ***')
+
+#create processed directory, in case it doesn't exist
+wifisIO.createDir('processed')
 
 #read file list
 lst= wifisIO.readAsciiList(fileList)
@@ -36,127 +53,156 @@ lst= wifisIO.readAsciiList(fileList)
 if lst.ndim == 0:
     lst = [lst]
 
-procFiles = []
+procFlux = []
+procSigma = []
+procSatFrame = []
+
 #go through list and process each file individually
 
-for folder in lst:
+#first check master flat and limits exists
+if(os.path.exists('processed/master_flat.fits') and os.path.exists('processed/master_flat_limits.fits') and os.path.exists('processed/master_flat_slices.fits')):
 
-    folder = folder.tostring()
-    savename = 'processed/'+folder
+    cont = wifisIO.userInput('Master flat, slices and limits files already exists, do you want to continue processing (y/n)?')
 
-    if(os.path.exists(savename+'_flat.fits')):
-        cont = wifisIO.userInput('Processed flatfield file already exists for ' +folder+', do you want to continue processing (y/n)?')
-        if (cont.lower() == 'n'):
-            print('Reading image'+savename+'_flat.fits instead')
-            fluxImg = wifisIO.readImgFromFile(savename+'_flat.fits')
-            contProc = False
-        else:
-            contProc = True
-    else:
+    if (cont.lower() == 'y'):
         contProc = True
-        
-    if (contProc):
-        #Read in data
-        ta = time.time()
-        data, inttime, hdr = wifisIO.readImgsFromFolder(folder)
-        print("time to read all files took", time.time()-ta, " seconds")
-        
-        nFrames = inttime.shape[0]
-        nx = data.shape[1]
-        ny = data.shape[0]
-
-        #******************************************************************************
-        #Correct data for reference pixels
-        ta = time.time()
-        print("Subtracting reference pixel channel bias")
-        refCor.channelCL(data, nFrames, 32)
-        print("Subtracting reference pixel row bias")
-        #refCor.row(data, nFrames, 4) # *** NOT CURRENTLY BEING USED ***
-        print("time to apply reference pixel corrections ", time.time()-ta, " seconds")
-        
-        #******************************************************************************
-        #find if any pixels are saturated to avoid use in future calculations
-        #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-        satFile = glob.glob('processed/*satCounts.fits')[0]
-        
-        satCounts = wifisIO.readImgFromFile(satFile)[0]
-        satFrame = satInfo.getSatFrameCL(data, satCounts,32)
-        
-        #******************************************************************************
-        #apply non-linearity correction
-        ta = time.time()
-        print("Correcting for non-linearity")
-        
-        #find NL coefficient file
-        #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-        nlFile = glob.glob('processed/*NLCoeff.fits')[0]
-        
-        nlCoeff = wifisIO.readImgFromFile(nlFile)[0]
-        NLCor.applyNLCorCL(data, nlCoeff, 32)
-        print("time to apply non-linearity corrections ", time.time()-ta, " seconds")
-        
-        #******************************************************************************
-        #Combine data cube into single image
-        fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)
-        data = 0
-        
-        #write image to a file
-        # Save saturation info as additional extension
-        out = np.zeros((ny,nx,2))
-        out[:,:,0] = fluxImg
-        out[:,:,1] = satFrame        
-        
-        #add additional header information here
-        
-        wifisIO.writeFits(out, savename+'_flat.fits', hdr=hdr)
-
-        out = 0
-        
-    procFiles.append(fluxImg)
-
-#now combine all flatfield images into master flat
-
-procFiles = np.array(procFiles)
-master = np.median(procFiles, axis=0)
-
-#******************************************************************************
-#STILL TO DO CORRECT BAD PIXELS
-# CODE GOES HERE
-#******************************************************************************
-
-#find limits of each slice
-limits = slices.findLimits(master, dispAxis=1, winRng=51, imgSmth=5, limSmth=10)
-
-#write limits to file
-if(os.path.exists('processed/'+'master_flat_limits.fits')):
-    cont = wifisIO.userInput('Master flat limits file already exists, do you want to replace (y/n)?')
-    if (cont.lower() == 'y'):
-        contWrite = True
     else:
-        contWrite = False
+        contProc = False
 else:
-    contWrite = True
-if (contWrite):
-    #add/modify header information here
-    wifisIO.writeFits(limits,'processed/'+'master_flat_limits.fits')
+    contProc = True
+    
+if (contProc):
+    
+    for folder in lst:
+        
+        folder = folder.tostring()
+        savename = 'processed/'+folder
 
-#now extract the individual slices
-extSlices = slices.extSlices(master, limits, dispAxis=1)
+        if(os.path.exists(savename+'_flat.fits')):
+            cont = wifisIO.userInput('Processed flat field file already exists for ' +folder+', do you want to continue processing (y/n)?')
+            if (cont.lower() == 'n'):
+                print('Reading image'+savename+'_flat.fits instead')
+                 fluxImg, sigmaImg, satFrame= wifisIO.readRampFromFile(savename+'_flat.fits')
+                
+                contProc2 = False
+            else:
+                contProc2 = True
+        else:
+            contProc2 = True
+        
+        if (contProc2):
+            #Read in data
+            ta = time.time()
+            data, inttime, hdr = wifisIO.readRampFromFolder(folder)
+            print("time to read all files took", time.time()-ta, " seconds")
+            
+            nFrames = inttime.shape[0]
+            nx = data.shape[1]
+            ny = data.shape[0]
+            
+            #******************************************************************************
+            #Correct data for reference pixels
+            ta = time.time()
+            print("Subtracting reference pixel channel bias")
+            refCor.channelCL(data, 32)
+            print("Subtracting reference pixel row bias")
+            refCor.rowCL(data, 4,1)
+            print("time to apply reference pixel corrections ", time.time()-ta, " seconds")       
+            #******************************************************************************
+            #find if any pixels are saturated to avoid use in future calculations
 
-#now get smoothed and normalized response function
-response = slices.getResponseAll(extSlices, 0, 0.01)
+            satCounts = wifisIO.readImgsFromFile(satFile)[0]
+            satFrame = satInfo.getSatFrameCL(data, satCounts,32)
+                
+            #******************************************************************************
+            #apply non-linearity correction
+            ta = time.time()
+            print("Correcting for non-linearity")
+            
+            #find NL coefficient file
+            nlCoeff = wifisIO.readImgsFromFile(nlFile)[0]
+            NLCor.applyNLCorCL(data, nlCoeff, 32)
+            print("time to apply non-linearity corrections ", time.time()-ta, " seconds")
+        
+            #******************************************************************************
+            #Combine data cube into single image
+            fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)[0]
+            #fluxImg = combData.upTheRampCRRejectCL(inttime, data, satFrame, 32)
 
-#write master image to file
-if(os.path.exists('processed/'+'master_flat.fits')):
-    cont = wifisIO.userInput('Master flat file already exists, do you want to replace (y/n)?')
-    if (cont.lower() == 'y'):
-        contWrite = True
+            data = 0
+            
+            #get uncertainties for each pixel
+            sigmaImg = wifisUncertainties.getUTR(inttime, fluxImg, satFrame)
+            
+            #write image to a file, saving saturation info as additional extension
+
+            #****************************************************************************
+            #add additional header information here
+
+            #****************************************************************************
+            
+            wifisIO.writeFits([fluxImg, sigma, satFrame], savename+'_flat.fits', hdr=hdr)
+            
+            out = 0
+        else:
+            #read in file instead
+            fluxImg,sigmaImg, satFrame = wifisReadImgsFromFile(savename+'_flat.fits')[0:3]
+
+        procFlux.append(fluxImg)
+        procSigma.append(sigmaImg)
+        procSatFrame.append(satFrame)
+
+
+    #*************************************************************************************
+    #*************************************************************************************
+    #*************************************************************************************
+    #DONE PROCESSING INDIVIDUAL FLAT FIELD RAMPS, NOW CREATE MASTER IMAGES
+    
+    #now combine all flatfield images into master flat, propagating uncertainties as needed
+    masterFlat, masterSigma = wifisUncertainties.compMedian(np.array(procFlux), np.array(procSigma),axis=0)
+
+    #combine satFrame
+    masterSatFrame = np.median(np.array(procSatFrame), axis=0).astype('int')
+    
+    #******************************************************************************
+    # CORRECT BAD PIXELS
+
+    #check for BPM and read, if exists
+    if(os.path.exists(bpmFile)):
+        masterFlatCor = badPixels.corBadPixelsAll(masterFlat, BPM, dispAxis=1, mxRng=2, MP=True)
+        masterSigmaCor = badPixels.corBadPixelsAll(mastSigma, BPM, dispAxis=1, mxRng=2, MP=True, sigma=True)
     else:
-        contWrite = False
-else:
-    contWrite = True
-if (contWrite):
-    #add/modify header information here
-    wifisIO.writeImgSlices(fluxImg, response,'processed/'+'master_flat.fits')
+        masterFlatCor = masterFlat
+        masterSigmaCor = masterSigma
+        
+        print('*** WARNING: No bad pixel mask provided ***')
+    #******************************************************************************
+    
+    #find limits of each slice
+    limits = slices.findLimits(masterFlatCor, dispAxis=1, winRng=51, imgSmth=5, limSmth=10)
 
+    #write limits to file
+    wifisIO.writeFits(limits,'processed/master_flat_limits.fits')
+
+    #now extract the individual slices
+    fluxSlices = slices.extSlices(masterFlatCor, limits, dispAxis=1)
+
+    #extract uncertainty slices
+    sigmaSlices = slices.extSlices(masterSigmaCor, limits, dispAxis=1)
+
+    #extract saturation slices
+    satSlices = slices.extSlices(masterSatFrame, limits, dispAxis=1)
+
+    #now get smoothed and normalized response function
+    masterRes = slices.getResponseAll(extSlices, 0, 0.1)
+    masterSig = slices.ffCorrectAll(sigmaSlices, masterRes)
+        
+    #write master image to file
+    wifisIO.writeFits([masterFlatCor,masterSigmaCor, masterSatFrame],'processed/'+'master_flat.fits')
+
+    #write master image slices to file
+    wifisIO.writeFits([masterRes, masterSig, satSlices]
+else:
+    print('No processing necessary')
+    
 print ("Total time to run entire script: ",time.time()-t0)
