4bf9530ac085a0b2ff82b080bb815c0eb3564dfd
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-05-08 13:56:58 -0400

    Modified default dispersion axis and added way to propagate errors.

--- core/wifisBadPixels.py
+++ core/wifisBadPixels.py
@@ -9,7 +9,7 @@ import wifisIO
 import matplotlib.pyplot as plt
 
 
-def corBadPixelsAll(data, BPM, dispAxis=1,mxRng=2,MP=True, ncpus=None):
+def corBadPixelsAll(data, BPM, dispAxis=0,mxRng=2,MP=True, ncpus=None, sigma=False):
     """
     """
 
@@ -30,11 +30,18 @@ def corBadPixelsAll(data, BPM, dispAxis=1,mxRng=2,MP=True, ncpus=None):
 
     if (MP == False):
         corrections=[]
-        for i in range(bad[0].shape[0]):
-            corrections.append(corBadPixel([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng]))
+
+        #run separate algorithm on uncertainty data
+        if (sigma):
+            for i in range(bad[0].shape[0]):
+                corrections.append(corBadPixelSigma([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng]))
+        else:
+            for i in range(bad[0].shape[0]):
+                corrections.append(corBadPixel([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng]))
     else:
         #setup input list to feed MP routine
         lst = []
+        
         for i in range(bad[0].shape[0]):
             lst.append([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng])
 
@@ -42,7 +49,10 @@ def corBadPixelsAll(data, BPM, dispAxis=1,mxRng=2,MP=True, ncpus=None):
         if (ncpus == None):
             ncpus =mp.cpu_count()
         pool = mp.Pool(ncpus)
-        corrections = pool.map(corBadPixel, lst)
+        if (sigma):
+            corrections = pool.map(corBadPixelSigma, lst)
+        else:
+            corrections = pool.map(corBadPixel, lst)
         pool.close()
 
     #apply corrections to input array in place
@@ -105,3 +115,52 @@ def corBadPixel(input):
     
     return corr
     
+def corBadPixelSigma(input):
+    """
+    Interpolate only along dispersion axis and only if pixels available within specified, progagating uncertainties.
+    """
+
+    data = input[0]
+    BPM = input[1]
+    pixy = input[2]
+    pixx = input[3]
+    mxRng = input[4]
+
+    nx = BPM.shape[1]
+    ny = BPM.shape[0]
+    
+    #zero pad current row
+    ytmp = np.zeros(nx+2)
+    np.copyto(ytmp[1:-1],data[pixy,:])
+    bpmTmp = np.zeros((nx+2), dtype='int')
+    np.copyto(bpmTmp[1:-1],BPM[pixy,:])
+
+    pixx += 1
+
+    #determine if any good pixels within range
+    xa = pixx
+    keepxa = False
+    for xa in xrange(pixx-1,pixx-mxRng-1,-1):
+        if(bpmTmp[xa] == 0):
+            keepxa = True
+            break
+
+    xb = pixx
+    keepxb = False
+    for xb in xrange(pixx+1,pixx+mxRng+1,1):
+        if (bpmTmp[xb] == 0):
+            keepxb = True
+            break
+
+    #interpolate between values, if useable, else put in a NaN
+    if (keepxa and keepxb):
+        ia = (xb-pixx)/float(xb-xa)*ytmp[xa]*keepxa
+        ib = (pixx-xa)/float(xb-xa)*ytmp[xb]*keepxa
+        corr = np.sqrt(ia**2 + ib**2)
+    else:
+        corr = np.nan
+
+    #restore pixx to input value, in case it modifies the input
+    pixx -= 1
+    
+    return corr
