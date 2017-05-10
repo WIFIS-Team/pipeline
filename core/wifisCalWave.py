e5744667b17bd3ddfff348c684927170cb8d693b
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-05-09 22:53:32 -0400

    modified to run with most recent version of pipeline

--- core/wifisCalWave.py
+++ core/wifisCalWave.py
@@ -22,22 +22,40 @@ import wifisIO
 import wifisCombineData as combData
 import wifisWaveSol as waveSol
 
-os.environ['PYOPENCL_COMPILER_OUTPUT'] = '1' # Used to show compile errors for debugging, can be removed
-os.environ['PYOPENCL_CTX'] = ':1' # Used to specify which OpenCL device to target
+os.environ['PYOPENCL_COMPILER_OUTPUT'] = '0' # Used to show compile errors for debugging, can be removed
+os.environ['PYOPENCL_CTX'] = '1' # Used to specify which OpenCL device to target
 
 #*****************************************************************************
 #************************** Required input ***********************************
 foldername = 'test1'
-templatename = 'template'
+templateFile = 'template'
+prevResultsFile = 'prevSolName'
+nlFile = 'processed/master_detLin_NLCoeff.fits'        
+satFile = 'processed/master_detLin_satCounts.fits'
+bpmFile = 'processed/bad_pixel_mask.fits'
+limitsFile = ''
 #*****************************************************************************
 #*****************************************************************************
+
+#first check if required input exists
+if not (os.path.exists(nlFile) and os.path.exists(satFile) and os.path.exists(limitsFile)):
+    if not (os.path.exists(satFile)):
+        print ('*** ERROR: Cannot continue, file ' + satFile + ' does not exist. Please process a detector linearity calibration sequence or provide the necessary file ***')
+    if not (os.path.exists(nlFile)):
+        print ('*** ERROR: Cannot continue, file ' + nlFile + ' does not exist. Please process a detector linearity calibration sequence or provide the necessary file ***')
+    if not (os.path.exists(limitsFile)):
+        print ('*** ERROR: Cannot continue, file ' + limitsFile + ' does not exist. Please process flat field calibration sequence or provide the necessary file ***')
+    raise SystemExit('*** Missing required calibration files, exiting ***')
+
+#create processed directory, in case it doesn't exist
+wifisIO.createDir('processed')
 
 savename = '/processed/'+filename
 
 t0 = time.time()
 
-if(os.path.exists(savename+'_waveCal.fits')):
-    cont = wifisIO.userInput('Processed waveCal file already exists for ' +foldername+', do you want to continue processing (y/n)?')
+if(os.path.exists(savename+'_waveCal.fits') and (os.path.exists(savename+'_waveMap.fits')) and (os.path.exists(savename+'_waveFitResuls.pkl'))):
+    cont = wifisIO.userInput('Processed wavelength calibration files already exists for ' +foldername+', do you want to continue processing (y/n)?')
     if (cont.lower() == 'y'):
         contProc = True
     else:
@@ -48,9 +66,9 @@ else:
 if (contProc):
     
     #Read in data
-    t1 = time.time()
-    data, inttime, hdr = wifisIO.readFromFolder(foldername)
-    print("time to read all files took", time.time()-t1, " seconds")
+    ta = time.time()
+    data, inttime, hdr = wifisIO.readRampFromFolder(foldername)
+    print("time to read all files took", time.time()-ta, " seconds")
 
     nFrames = inttime.shape[0]
     nx = data.shape[1]
@@ -60,16 +78,14 @@ if (contProc):
     #Correct data for reference pixels
     ta = time.time()
     print("Subtracting reference pixel channel bias")
-    #refCor.channelCL(data, nFrames, 32)
+    refCor.channelCL(data, 32)
     print("Subtracting reference pixel row bias")
-    #refCor.row(data, nFrames, 4)
+    refCor.rowCL(data, 4,5)
     print("time to apply reference pixel corrections ", time.time()-ta, " seconds")
     #******************************************************************************
 
     #find if any pixels are saturated to avoid use in future calculations
-    #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-    satFile = glob.glob('processed/*satCounts.fits')[0]
-    satCounts = wifisIO.readImgFromFile(satFile)
+    satCounts = wifisIO.readImgsFromFile(satFile)
     satFrame = satInfo.getSatFrameCL(data, satCounts,32)
     #******************************************************************************
 
@@ -78,27 +94,26 @@ if (contProc):
     print("Correcting for non-linearity")
 
     #find NL coefficient file
-    #*** ADD CODE TO DEAL WITH POTENTIAL OF MULTIPLE NL FILES, BUT FOR NOW JUST TAKE THE FIRST
-    nlFile = glob.glob('processed/*NLCoeff.fits')[0]
-
-    nlCoeff = wifisIO.readImgFromFile(nlFile)
+    nlCoeff = wifisIO.readImgsFromFile(nlFile)
     NLCor.applyNLCorCL(data, nlCoeff, 32)
     print("time to apply non-linearity corrections ", time.time()-ta, " seconds")
 
     #******************************************************************************
 
     #Combine data into single image
-    fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)
-
+    fluxImg = combData.upTheRampCL(inttime, data, satFrame, 32)[0]
+    sigmaImg = wifisUncertainties.getUTR(inttime, fluxImg, satFrame)
     data = 0
 
     #******************************************************************************
     #Correct for dark current
     #Identify appropriate dark image for subtraction
-    darkName = 'processed/master_dark_I'+str(inttime[-1])+'.fits'
+    iTime = inttime[-1]-inttime[0]
+    darkName = 'processed/master_dark_I'+str(iTime)+'.fits'
     if (os.path.exists(darkName)):
-        darkImg = wifisIO.readImgFromFile(darkName)[:,:,0] #only use the first HDU, the second contains saturation information
-        flxImg = flxImg - darkImg
+        darkImg,darkSig = wifisIO.readImgsFromFile(darkName)[0][0,1] #get the first two extensions
+        fluxImg -= darkImg
+        sigmaImg = np.sqrt(sigmaImg**2 + darkSig**2)
     else:
         cont = wifisIO.userInput('No corresponding master dark image could be found, do you want to proceed without dark subtraction (y/n)?')
         if (cont.lower() == 'n'):
@@ -107,59 +122,42 @@ if (contProc):
     #******************************************************************************    
     #write image to a file
 
-    if(os.path.exists(savename+'_waveCal.fits')):
-        cont = wifisIO.userInput('Processed waveCal file already exists for ' +foldername+', do you want to replace (y/n)?')
-        if (cont.lower() == 'y'):
-            contWrite = True
-        else:
-            contWrite = False
-    else:
-        contWrite = True
-        
-
-    if (contWrite):
-        #add or modify fits header here
-
-        out = np.zeros((ny,nx,2))
-        out[:,:,0] = fluxImg
-        out[:,:,1] = satFrame
-        wifisIO.writeFits(out, savename+'_waveCal.fits', hdr=hdr,'Total integration time'])
-        out = 0
+    wifisIO.writeFits([fluxImg, sigmaImg, satFrame], savename+'_waveCal.fits')
         
     #******************************************************************************
     #Determine dispersion solution
 
     #read in template
-    template = wifisIO.readImgFromFile(templatename+'_waveCal.fits')
+    template = wifisIO.readImgsFromFile(templateFile)
 
+    #read in template results to extract lambda -> wavelength solution
+    prevResults = wifisIO.readPickle(prevResultsFile)
+    prevSol = prevResults[5]
+    
     #provide line atlas file
     atlasname = 'external_data/best_lines2.dat'
 
-    #read in dispersion solution corresponding to template
-    prevSol = wifisIO.readTable(templatename+'_waveSol.dat')
-
     #check if solution already exists.
-    if(os.path.exists(savename+'_waveSol.dat') and (os.path.exists(savename+'_waveMap.fits'))):
-        cont = wifisIO.userInput('Dispersion solution already exists for ' +foldername+', do you want to continue and replace (y/n)?')
+    if(os.path.exists(savename+'_waveFitResults.pkl') and (os.path.exists(savename+'_waveMap.fits'))):
+        cont = wifisIO.userInput('Dispersion solution and wavemap already exists for ' +foldername+', do you want to continue and replace (y/n)?')
         if (cont.lower() == 'n'):
             exit()
 
-    #data image, template, atlas file, max fitting order, list of prev. solutions, dispersion axis direction, window range fro line fitting, maximum allowable cross-correlation pixel offset
-    result = waveSol.getWaveSol(fluxImg, template, atlasname, 1, prevSol, dispAxis=1, winRng=7, mxCcor=30, weights=False, buildSol=False)
+    #first extract the slices
+    limits = wifisIO.readImgsFromFile(limitsFile)
+    waveSlices = slices.extSlices(fluxImg, limits, dispAxis=0)
+    
+    result = waveSol.getWaveSol(waveSlices, template, atlasname, 3, prevSol, winRng=9, mxCcor=30, weights=False, buildSol=False, sigmaClip=2, allowLower=True, lngthConstraint=True)
 
-    #Save dispersion solution
-    dispSol = []
-    for sol in result:
-        dispSol.append(sol[0])
-
-    wifisIO.writeTable(savename+'_waveSol.dat', dispSol)
+    dispSolLst = result[0]
+    #Save all results
+    wifisIO.writePickle(savename+'_waveFitResults.pkl', results)
 
     #Create wavemap
-    waveMap = waveSol.buildWaveMap(fluxImg, dispSol, dispAxis=1)
+    waveMapLst = waveSol.buildWaveMap(dispSolLst, waveSlices[0].shape[1])
 
     #save wavemap solution
-    wifisIO.writeFits(waveMap, savename+'_waveMap.fits')
+    wifisIO.writeFits(waveMapLst, savename+'_waveMap.fits')
 
-t1 = time.time()
-print ("Total time to run entire script: ",t1-t0)
+print ("Total time to run entire script: ",time.time()-t0)
 
