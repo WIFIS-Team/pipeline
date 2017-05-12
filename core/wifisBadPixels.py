dd14873e2977fa6f92473ed3a29e88225224512d
Jason Grunhut <jgrunhut@localhost.localdomain>
2017-05-11 23:28:16 -0400

    Fixed major bug from last version where good pixels were being corrected. Also modified behaviour to improve performance and memory usage.

--- core/wifisBadPixels.py
+++ core/wifisBadPixels.py
@@ -16,17 +16,17 @@ def corBadPixelsAll(data, BPM, dispAxis=0,mxRng=2,MP=True, ncpus=None, sigma=Fal
     #run through list of all bad pixels and correct them through linear interpolation along dispersion axis
 
     #create copies of input data array
-    tmpData = np.empty(data.shape, dtype=data.dtype)
-    np.copyto(tmpData, data)
-    tmpBPM = np.zeros(BPM.shape, dtype='int')
-    np.copyto(tmpBPM, BPM.astype('int'))
+    #tmpData = np.empty(data.shape, dtype=data.dtype)
+    #np.copyto(tmpData, data)
+    #tmpBPM = np.zeros(BPM.shape, dtype='int')
+    #np.copyto(tmpBPM, BPM.astype('int'))
     
-    if (dispAxis==0):
-        tmpBPM = tmpBPM.T
-        tmpData = tmpData.T
+    #if (dispAxis==0):
+    #    tmpBPM = tmpBPM.T
+    #    tmpData = tmpData.T
            
     #create list of bad pixels
-    bad = np.where(tmpBPM != 1)
+    bad = np.where(BPM != 0)
 
     if (MP == False):
         corrections=[]
@@ -34,16 +34,27 @@ def corBadPixelsAll(data, BPM, dispAxis=0,mxRng=2,MP=True, ncpus=None, sigma=Fal
         #run separate algorithm on uncertainty data
         if (sigma):
             for i in range(bad[0].shape[0]):
-                corrections.append(corBadPixelSigma([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng]))
+                if (dispAxis==0):
+                    #only provide the correction routine with the disperion vector for that particular bad pixel
+                    corrections.append(corBadPixelSigma([data[:,bad[1][i]], BPM[:,bad[1][i]], bad[0][i], mxRng]))
+                else:
+                    corrections.append(corBadPixelSigma([data[bad[0][i],:], BPM[bad[0][i],:], bad[1][i], mxRng]))
         else:
             for i in range(bad[0].shape[0]):
-                corrections.append(corBadPixel([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng]))
+                #only provide the correction routine with the disperion vector for that particular bad pixel
+                if (dispAxis==0):
+                    corrections.append(corBadPixel([data[:,bad[1][i]], BPM[:,bad[1][i]], bad[0][i], mxRng]))
+                else:
+                    corrections.append(corBadPixel([data[bad[0][i],:], BPM[bad[0][i],:], bad[1][i], mxRng]))
     else:
         #setup input list to feed MP routine
         lst = []
         
         for i in range(bad[0].shape[0]):
-            lst.append([tmpData, tmpBPM, bad[0][i],bad[1][i], mxRng])
+            if (dispAxis==0):
+                lst.append([data[:,bad[1][i]], BPM[:,bad[1][i]], bad[0][i], mxRng])
+            else:
+                lst.append([data[bad[0][i],:], BPM[bad[0][i],:], bad[1][i], mxRng])
 
         #run MP routine
         if (ncpus == None):
@@ -55,15 +66,18 @@ def corBadPixelsAll(data, BPM, dispAxis=0,mxRng=2,MP=True, ncpus=None, sigma=Fal
             corrections = pool.map(corBadPixel, lst)
         pool.close()
 
-    #apply corrections to input array in place
+    #create new array with corrected pixels
+    outData = np.empty(data.shape, dtype=data.dtype)
+    np.copyto(outData, data)
+    
     for i in range(len(corrections)):
-        tmpData[bad[0][i],bad[1][i]] = corrections[i]
+        outData[bad[0][i],bad[1][i]] = corrections[i]
 
-    # reorient if necessary
-    if (dispAxis==0):
-        tmpData = tmpData.T
+   # # reorient if necessary
+   # if (dispAxis==0):
+   #     tmpData = tmpData.T
         
-    return tmpData
+    return outData
 
 def corBadPixel(input):
     """
@@ -72,46 +86,43 @@ def corBadPixel(input):
 
     data = input[0]
     BPM = input[1]
-    pixy = input[2]
-    pixx = input[3]
-    mxRng = input[4]
+    badPix = input[2]
+    mxRng = input[3]
 
-    nx = BPM.shape[1]
-    ny = BPM.shape[0]
-    
-    #zero pad current row
+    nx = data.shape[0]
+    #zero pad current vector
     ytmp = np.zeros(nx+2)
-    np.copyto(ytmp[1:-1],data[pixy,:])
+    np.copyto(ytmp[1:-1],data)
     bpmTmp = np.zeros((nx+2), dtype='int')
-    np.copyto(bpmTmp[1:-1],BPM[pixy,:])
+    np.copyto(bpmTmp[1:-1],BPM)
 
-    pixx += 1
+    badPix += 1
 
     #determine if any good pixels within range
-    xa = pixx
+    xa = badPix
     keepxa = False
-    for xa in xrange(pixx-1,pixx-mxRng-1,-1):
+    for xa in xrange(badPix-1,badPix-mxRng-1,-1):
         if(bpmTmp[xa] == 0):
             keepxa = True
             break
 
-    xb = pixx
+    xb = badPix
     keepxb = False
-    for xb in xrange(pixx+1,pixx+mxRng+1,1):
+    for xb in xrange(badPix+1,badPix+mxRng+1,1):
         if (bpmTmp[xb] == 0):
             keepxb = True
             break
 
     #interpolate between values, if useable, else put in a NaN
     if (keepxa and keepxb):
-        ia = (xb-pixx)/float(xb-xa)*ytmp[xa]*keepxa
-        ib = (pixx-xa)/float(xb-xa)*ytmp[xb]*keepxa
+        ia = (xb-badPix)/float(xb-xa)*ytmp[xa]*keepxa
+        ib = (badPix-xa)/float(xb-xa)*ytmp[xb]*keepxa
         corr = ia + ib
     else:
         corr = np.nan
 
     #restore pixx to input value, in case it modifies the input
-    pixx -= 1
+    badPix -= 1
     
     return corr
     
