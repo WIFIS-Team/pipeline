9d227464afd79c66fddf15efe93cc08761d41dc9
jgrunhut <jason.grunhut@gmail.com>
2017-06-02 15:42:40 -0400

    Bug fixes

--- check_scripts/quick_reduction.py
+++ check_scripts/quick_reduction.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wifisSlices as slices
 import wifisCreateCube as createCube
-import wifisCombineData as combineData
+import wifisCombineData as combData
 import wifisHeaders as headers
 import wifisGetSatInfo as satInfo
 from astropy import wcs
+import os
+
+os.environ['PYOPENCL_CTX'] = '1' # Used to specify which OpenCL device to target. Should be uncommented and pointed to correct device to avoid future interactive requests
 
 #******************************************************************************
 #required user input
-rampFolder = '' #must point to location of folder containing the ramp
-distMapFile = '' #must point to location of distortion map file
-distLimitsFile = '' #must point to the location of the flat-field associated with the Ronchi mask image
-limitsFile = '' #must point to location of limits file, corresponding to this observation
-satFile = '' #must point to location of saturation limits file, from detector linearity measurements
+rampFolder = '20170511005013' #must point to location of folder containing the ramp
+distMapFile = '/home/jason/wifis/static_processed/ronchi_map_polyfit.fits' #must point to location of distortion map file
+distLimitsFile = '/home/jason/wifis/static_processed/master_flat_limits.fits' #must point to the location of the flat-field associated with the Ronchi mask image
+limitsFile = 'processed/20170510233851_flat_limits.fits' #must point to location of limits file, corresponding to this observation
+satFile = '/data/WIFIS/H2RG-G17084-ASIC-08-319/UpTheRamp/20170504201819/processed/master_detLin_satCounts.fits' #must point to location of saturation limits file, from detector linearity measurements
+spatGridProps = wifisIO.readTable('/home/jason/wifis/static_processed/spatGridProps.dat')
+#******************************************************************************
 
 #read in data
 data, inttime, hdr = wifisIO.readRampFromFolder(rampFolder)
@@ -36,8 +41,8 @@ shft = np.median(limits[1:-1, :] - distLimits[1:-1,:])
 dataSlices = slices.extSlices(fluxImg, distLimits, dispAxis=0, shft=shft)
 
 #place on uniform spatial grid
-distMap = wifisIO.readImgsFromFile(distMapFile)
-dataGrid = createCube.distCorAll(dataSlices, distMap)
+distMap = wifisIO.readImgsFromFile(distMapFile)[0]
+dataGrid = createCube.distCorAll(dataSlices, distMap, spatGridProps=spatGridProps)
 
 #create cube
 dataCube = createCube.mkCube(dataGrid, ndiv=1)
@@ -47,7 +52,7 @@ dataImg = createCube.collapseCube(dataCube)
 
 #fill in header info
 obsinfoFile = rampFolder+'/obsinfo.dat'
-headers.addTelInfo(hdr, obsInfoFile)
+headers.addTelInfo(hdr, obsinfoFile)
 
 #set the pixel scale
 xScale = 0.532021532706
