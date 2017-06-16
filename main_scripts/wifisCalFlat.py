47431258d0582a118229fe5df1a8d1ea724fb529
jgrunhut <jason.grunhut@gmail.com>
2017-06-15 21:50:51 -0400

    general improvements to match current pipeline

--- main_scripts/wifisCalFlat.py
+++ main_scripts/wifisCalFlat.py
@@ -8,7 +8,8 @@ Produces:
 - ??
 
 """
-
+import matplotlib
+matplotlib.use('pdf')
 import numpy as np
 import time
 import matplotlib.pyplot as plt
@@ -37,14 +38,18 @@ t0 = time.time()
 fileList = 'flat.lst' # a simple ascii file containing a list of the folder names that contain the ramp data
 
 #mostly static input from here
-nlFile = '/data/WIFIS/H2RG-G17084-ASIC-08-319/UpTheRamp/20170504201819/processed/master_detLin_NLCoeff.fits' # the non-linearity correction coefficients file        
-satFile = '/data/WIFIS/H2RG-G17084-ASIC-08-319/UpTheRamp/20170504201819/processed/master_detLin_satCounts.fits' # the saturation limits file
+rootFolder = '/data/WIFIS/H2RG-G17084-ASIC-08-319'
+nlFile = '/home/jason/wifis/data/non-linearity/may/processed/master_detLin_NLCoeff.fits' # the non-linearity correction coefficients file        
+satFile = '/home/jason/wifis/data/non-linearity/may/processed/master_detLin_satCounts.fits' # the saturation limits file
 bpmFile = '/data/pipeline/external_data/bpm.fits' # the bad pixel mask
 distMapLimitsFile = '/data/pipeline/external_data/ronchiMap_limits.fits'
 
 #optional behaviour of pipeline
 plot = True #whether to plot the traces
 crReject = False
+skipObsinfo = False
+skipMissing = True
+
 #*****************************************************************************
 
 #NOTES
@@ -112,80 +117,117 @@ for lstNum in range(len(lst)):
             cont = wifisIO.userInput('Processed flat field file already exists for ' +folder+', do you want to continue processing (y/n)?')
             if (cont.lower() == 'n'):
                 print('Reading image'+savename+'_flat.fits instead')
-                flatCor, sigmaImg, satFrame= wifisIO.readImgsFromFile(savename+'_flat.fits')[0]
+                flatImgs, hdr= wifisIO.readImgsFromFile(savename+'_flat.fits')
+                flatImg, sigmaImg, satFrame = flatImgs
+                if (type(hdr) is list):
+                    hdr = hdr[0]
+                contProc2=False
+            else:
+                contProc2=True
+        else:
+            contProc2=True
+
+        if contProc2:
+            print('Processing ramp')
+            flatImg, sigmaImg, satFrame, hdr = processRamp.auto(folder, rootFolder,savename+'_flat.fits', satCounts, nlCoeff, BPM, nChannel=32, rowSplit=1, nlSplit=32, combSplit=32, crReject=crReject, bpmCorRng=20, skipObsinfo=skipObsinfo, skipMissing=skipMissing)
+            
+        if os.path.exists(savename+'_flat_limits.fits'):
+            cont = wifisIO.userInput('Limits file already exists for ' +folder+', do you want to continue processing (y/n)?')
+            if (cont.lower() == 'n'):
+                print('Reading limits'+savename+'_flat_limits.fits instead')
+                limits, limitsHdr= wifisIO.readImgsFromFile(savename+'_flat_limits.fits')
+                shft = limitsHdr['LIMSHIFT']
                 contProc2 = False
             else:
                 contProc2 = True
         else:
-            contProc2 = True
-        
+            contProc2= True
+
         if (contProc2):
-            if (os.path.exists(folder+'/Result')):
-                #process using Fowler/CDS routines
-                flatCor, sigmaCor, satFrame, hdr = processRamp.fromFowler(folder, savename+'_flat.fits', satCounts, nlCoeff, BPM, nChannel=32, rowSplit=1, nlSplit=1, combSplit=1, crReject=False, bpmCorRng=20)
+            print('Finding slice limits and extracting slices')
+            #find limits of each slice with the reference pixels, but the returned limits exclude them
+            limits = slices.findLimits(flatImg, dispAxis=0, winRng=51, imgSmth=5, limSmth=20, rmRef=True)
+
+            #get smoother limits, if desired, using polynomial fitting
+            polyLimits = slices.polyFitLimits(limits, degree=3)
+
+            if os.path.exists(distMapLimitsFile):
+                print('Finding slice limits relative to distortion map file')
+                distMapLimits = wifisIO.readImgsFromFile(distMapLimitsFile)[0]
+                shft = int(np.nanmedian(polyLimits[1:-1,:] - distMapLimits[1:-1,:]))
+                finalLimits = distMapLimits
             else:
-                flatCor, sigmaCor, satFrame, hdr = processRamp.fromUTR(folder, savename+'_flat.fits', satCounts, nlCoeff, BPM, nChannel=32, rowSplit=1, nlSplit=32, combSplit=32, crReject=crReject, bpmCorRng=20)
+                finalLimits = polyLimits
+                shft = 0
 
-        print('Finding slice limits and extracting slices')
-        #find limits of each slice with the reference pixels, but the returned limits exclude them
-        limits = slices.findLimits(flatCor, dispAxis=0, winRng=51, imgSmth=5, limSmth=20, rmRef=True)
-
-        #get smoother limits, if desired, using polynomial fitting
-        polyLimits = slices.polyFitLimits(limits, degree=3)
+            #write distMapLimits + shft to file
+            hdr.set('LIMSHIFT',shft, 'Limits shift relative to Ronchi slices')
+            wifisIO.writeFits(finalLimits,savename+'_flat_limits.fits', hdr=hdr)
+            
+            #save figures of tracing results for quality control purposes
+            if (plot):
+                print('Plotting results')
+                plt.ioff()
+                wifisIO.createDir('quality_control')
+                with PdfPages('quality_control/'+folder+'_flat_slices_traces.pdf') as pdf:
+                    fig = plt.figure()
+                    med1= np.nanmedian(flatImg)
+                    
+                    plt.imshow(flatImg[4:-4,4:-4], aspect='auto', cmap='jet', clim=[0,2.*med1], origin='lower')
+                    plt.colorbar()
+                    for l in range(limits.shape[0]):
+                        plt.plot(limits[l], np.arange(limits.shape[1]),'k', linewidth=1)
+                        plt.plot(finalLimits[l]+shft, np.arange(limits.shape[1]),'r--', linewidth=1)
+                    pdf.savefig()
+                    plt.close(fig)
 
         #get rid of reference pixels
-        flatCor = flatCor[4:2044, 4:2044]
-        sigmaCor = sigmaCor[4:2044, 4:2044]
-        satFrame = satFrame[4:2044,4:2044]
-                
-        if os.path.exists(distMapLimitsFile):
-            print('Using slice limits relative to distortion map file')
-            distMapLimits = wifisIO.readImgsFromFile(distMapLimitsFile)[0]
-            shft = int(np.nanmedian(polyLimits[1:-1,:] - distMapLimits[1:-1,:]))
-            finalLimits = distMapLimits
+        flatImg = flatImg[4:-4, 4:4-4]
+        sigmaImg = sigmaImg[4:-4, 4:-4]
+        satFrame = satFrame[4:-4,4:-4]
+
+        if os.path.exists(savename+'_flat_slices.fits'):
+            cont = wifisIO.userInput('Flat slices file already exists for ' +folder+', do you want to continue processing (y/n)?')
+            if (cont.lower() == 'n'):
+                print('Reading slices file '+savename+'_flat_slices.fits instead')
+                flatSlices = wifisIO.readImgsFromFile(savename+'_flat_slices.fits')[0]
+                contProc2 = False
+            else:
+                contProc2 = True
         else:
-            finalLimits = polyLimits
-            shft = 0
+            contProc2= True
             
-        #save figures of tracing results for quality control purposes
-        if (plot):
-            print('Plotting results')
-            #with PdfPages('quality_control/'+folder+'_flat_slices_traces.pdf') as pdf:
-            plt.ioff()
-            fig = plt.figure()
-            med1= np.nanmedian(flatCor)
-                
-            plt.imshow(flatCor, aspect='auto', cmap='jet', clim=[0,2.*med1], origin='lower')
-            plt.colorbar()
-            for l in range(limits.shape[0]):
-                plt.plot(limits[l], np.arange(limits.shape[1]),'k', linewidth=1)
-                plt.plot(finalLimits[l]+shft, np.arange(limits.shape[1]),'r--', linewidth=1)
-            plt.savefig('quality_control/'+folder+'_flat_slices_traces.png', dpi=300)
-            plt.close(fig)
+        if (contProc2):
+            print('Extracting slices')           
+            #now extract the individual slices
+            flatSlices = slices.extSlices(flatImg, finalLimits, dispAxis=0, shft=shft)
+            
+            #extract uncertainty slices
+            sigmaSlices = slices.extSlices(sigmaImg, finalLimits, dispAxis=0, shft=shft)
 
-        #write distMapLimits + shft to file
-        hdr.set('LIMSHIFT',shft, 'Limits shift relative to Ronchi slices')
-        wifisIO.writeFits(distMapLimits,savename+'_flat_limits.fits', hdr=hdr)
-      
-        #now extract the individual slices
-        flatSlices = slices.extSlices(flatCor, finalLimits, dispAxis=0, shft=shft)
+            #extract saturation slices
+            satSlices = slices.extSlices(satFrame, finalLimits, dispAxis=0)
 
-        #extract uncertainty slices
-        sigmaSlices = slices.extSlices(sigmaCor, finalLimits, dispAxis=0, shft=shft)
+            #write slices to file
+            wifisIO.writeFits(flatSlices+sigmaSlices+satSlices,savename+'_flat_slices.fits',hdr=hdr)
 
-        #extract saturation slices
-        satSlices = slices.extSlices(satFrame, finalLimits, dispAxis=0)
-
-        print('Getting normalized flat field')
-        #now get smoothed and normalized response function
-        flatNorm = slices.getResponseAll(flatSlices, 0, 0.1)
-        sigmaNorm = slices.ffCorrectAll(sigmaSlices, flatNorm)
-        
-        #write slices to file
-        wifisIO.writeFits(flatSlices+sigmaSlices+satSlices,savename+'_flat_slices.fits',hdr=hdr)
-
-        #write normalized images to file
-        wifisIO.writeFits(flatNorm + sigmaNorm + satSlices,savename+'_flat_slices_norm.fits',hdr=hdr)
+        if os.path.exists(savename+'_flat_slices_norm.fits'):
+            cont = wifisIO.userInput('Normalized flat slices file already exists for ' +folder+', do you want to continue processing (y/n)?')
+            if (cont.lower() == 'n'):
+                contProc2 = False
+            else:
+                contProc2 = True
+        else:
+            contProc2= True
+            
+        if (contProc2):
+            print('Getting normalized flat field')
+            #now get smoothed and normalized response function
+            flatNorm = slices.getResponseAll(flatSlices, 0, 0.1)
+            sigmaNorm = slices.ffCorrectAll(sigmaSlices, flatNorm)
+            
+            #write normalized images to file
+            wifisIO.writeFits(flatNorm + sigmaNorm + satSlices,savename+'_flat_slices_norm.fits',hdr=hdr)
         print('*** Finished processing ' + folder + ' in ' + str(time.time()-t1) + ' seconds ***')
         
 print('Total time to run entire script: ' + str(time.time()-t0) + ' seconds')
