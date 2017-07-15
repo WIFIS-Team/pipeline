cb9cd6454c55b0e55cf3774481454d265d7a9e0f
jgrunhut <jason.grunhut@gmail.com>
2017-07-14 22:03:05 -0400

    renamed get_disp_map to get_arc_width_map

---
+++ check_scripts/get_arc_width_map.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.use('gtkagg')
+import wifisIO
+import wifisSlices as slices
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+import os
+import warnings
+import wifisQuickReduction as quickReduction
+
+os.environ['PYOPENCL_CTX'] = '1' # Used to specify which OpenCL device to target. Should be uncommented and pointed to correct device to avoid future interactive requests
+
+#****************************************************************************************
+#REQUIRED INPUT FILES
+waveLstFile = 'wave.lst'
+flatLstFile = 'flat.lst'
+
+hband = False
+
+#****************************************************************************************
+
+waveLst = wifisIO.readAsciiList(waveLstFile)
+if waveLst.ndim==0:
+    waveLst = np.asarray([waveLst])
+
+flatLst = wifisIO.readAsciiList(flatLstFile)    
+if flatLst.ndim == 0:
+    flatLst = np.asarray([flatLst])
+
+quickReduction.initPaths()
+for fle in range(len(waveLst)):
+    quickReduction.procArcData(waveLst[fle], flatLst[fle], colorbarLims=[0,10]])
+    
