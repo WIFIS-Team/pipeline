d31d73cf68b93cb3ba4f238c37c8f7d04f810a1b
jgrunhut <jason.grunhut@gmail.com>
2017-10-20 11:32:26 -0400

    removed variable definition from script and modified names of some variables

--- main_scripts/wifisCalWave.py
+++ main_scripts/wifisCalWave.py
@@ -30,56 +30,11 @@ import wifisCalWaveFunc as calWave
 import colorama
 
 colorama.init()
+
+#************************** user input ***********************************
 #INPUT VARIABLE FILE NAME
 varFile = 'wifisConfig.inp'
 
-#*****************************************************************************
-#************************** user input ***********************************
-#initialize all variables.
-#DO NOT CHANGE VALUES HERE, EDIT THE 'variables.inp' FILE, WHICH OVERWRITES VALUES HERE
-
-waveLstFile = 'wave.lst' 
-flatLstFile = 'flat.lst'
-hband = False
-rootFolder = '/data/WIFIS/H2RG-G17084-ASIC-08-319'
-pipelineFolder = '/data/pipeline/'
-templateFile = pipelineFolder+'external_data/waveTemplate.fits'
-prevResultsFile = pipelineFolder+'external_data/waveTemplateFittingResults.pkl'
-distMapFile = pipelineFolder+'external_data/distMap.fits'
-spatGridPropsFile = pipelineFolder+'external_data/distMap_spatGridProps.dat'
-distMapLimitsFile = pipelineFolder+'external_data/distMap_limits.fits'
-darkFile = None
-mxOrder=3
-nlFile = pipelineFolder + 'external_data/master_detLin_NLCoeff.fits' # the non-linearity correction coefficients file        
-satFile = pipelineFolder+'external_data/master_detLin_satCounts.fits' # the saturation limits file
-bpmFile = pipelineFolder+'external_data/bpm.fits' # the bad pixel mask   
-atlasFile = pipelineFolder+'external_data/best_lines2.dat'
-#optional behaviour
-plot = True
-crReject = False
-cleanDispSol = True
-skipObsinfo = False
-#flat field specific options
-flatWinRng = 51
-flatImgSmth = 5
-flatPolyFitDegree=2
-cleanDispThresh = 1.5
-waveTrimThresh = 0
-sigmaClipRounds=2 #number of iterations when sigma-clipping of dispersion solution
-sigmaClip = 2 #sigma-clip cutoff when sigma-clipping dispersion solution
-sigmaLimit= 3 #relative noise limit (x * noise level) for which to reject lines
-
-#parameters used for processing of ramps
-nChannel=32 #specifies the number of channels used during readout of detector
-flatbpmCorRng=20 #specifies the maximum separation of pixel search to use during bad pixel correction
-nRowsAvg=4 # specifies the number of rows of reference pixels to use to correct for row bias (+/- nRowsAvg)
-nRowSplit=1 # specifies how many processing steps to use during reference row correction. Must be integer multiple of number of frames. For very long ramps, use a higher number to avoid OpenCL issues and/or high memory consumption.
-nlSplit=32 #specifies how many processing steps to use during non-linearity correction. Must be integer multiple of detector width. For very long ramps, use a higher number to avoid OpenCL issues and/or high memory consumption. 
-nCombSplit=32 #specifies how many processing steps to use during creation of ramp image. Must be integer multiple of detector width. For very long ramps, use a higher number to avoid OpenCL issues and/or high memory consumption.
-dispAxis=0
-obsCoords = [-111.600444444,31.9629166667,2071]
-
-#*****************************************************************************
 #*****************************************************************************
 
 logfile = open('wifis_reduction_log.txt','a')
@@ -168,7 +123,8 @@ if not (os.path.exists(spatGridPropsFile)):
 wifisIO.createDir('processed')
 wifisIO.createDir('quality_control')
 
-calWave.runCalWave(waveLst, flatLst, hband=hband, darkLst=darkLst, rootFolder=rootFolder, nlCoef=nlCoef, satCounts=satCounts, BPM=BPM, distMapLimitsFile=distMapLimitsFile, plot=plot, nChannel=nChannel, nRowsAvg=nRowsAvg, rowSplit=nRowSplit, nlSplit=nlSplit, combSplit=nCombSplit, bpmCorRng=flatbpmCorRng, crReject=crReject, skipObsinfo=skipObsinfo, flatWinRng=flatWinRng,flatImgSmth=flatImgSmth, flatPolyFitDegree=3, distMapFile=distMapFile, spatGridPropsFile=spatGridPropsFile, atlasFile=atlasFile, templateFile=templateFile, prevResultsFile=prevResultsFile,  sigmaClip=sigmaClip, sigmaClipRounds=sigmaClipRounds, sigmaLimit=sigmaLimit, cleanDispSol=cleanDispSol,cleanDispThresh = cleanDispThresh, waveTrimThresh=waveTrimThresh,nlFile=nlFile,satFile=satFile,bpmFile=bpmFile, obsCoords=obsCoords, dispAxis=dispAxis, darkFile=darkFile, logfile=logfile, mxOrder=mxOrder)
-    
+calWave.runCalWave(waveLst, flatLst, hband=hband, darkLst=darkLst, rootFolder=rootFolder, nlCoef=nlCoef, satCounts=satCounts, BPM=BPM, distMapLimitsFile=distMapLimitsFile, plot=waveSolPlot, nChannel=nChannel, nRowsAvg=nRowsAvg, rowSplit=nRowSplitWave, nlSplit=nlSplit, combSplit=nCombSplit, bpmCorRng=flatbpmCorRng, crReject=crReject, skipObsinfo=skipObsinfo, flatWinRng=flatWinRng,flatImgSmth=flatImgSmth, flatPolyFitDegree=3, distMapFile=distMapFile, spatGridPropsFile=spatGridPropsFile, atlasFile=atlasFile, templateFile=waveTempFile, prevResultsFile=waveTempResultsFile,  sigmaClip=sigmaClip, sigmaClipRounds=sigmaClipRounds, sigmaLimit=sigmaLimit, cleanDispSol=cleanDispSol,cleanDispThresh = cleanDispThresh, waveTrimThresh=waveTrimThresh,nlFile=nlFile,satFile=satFile,bpmFile=bpmFile, obsCoords=obsCoords, dispAxis=dispAxis, darkFile=darkFile, logfile=logfile, mxOrder=mxOrder, waveSmooth=waveSmooth, waveSolMP=waveSolMP,waveSolPlot=waveSolPlot, winRng=waveWinRng, nRowSplitFlat=nRowSplitFlat)
+
+logfile.write('********************\n')
 logfile.write('\n')
 logfile.close()
