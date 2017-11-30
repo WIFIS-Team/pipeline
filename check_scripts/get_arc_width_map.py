a6754e1406d225c92a1bbebe21527ccc9b0be022
jgrunhut <jason.grunhut@gmail.com>
2017-11-30 16:49:50 -0500

    updated to be compatible with newest version of the pipeline

--- check_scripts/get_arc_width_map.py
+++ check_scripts/get_arc_width_map.py
@@ -1,25 +1,21 @@
-import matplotlib
-matplotlib.use('gtkagg')
+#import matplotlib
+#matplotlib.use('gtkagg')
 import wifisIO
-import wifisSlices as slices
-import matplotlib.pyplot as plt
+#import wifisSlices as slices
+#import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.backends.backend_pdf import PdfPages
+#from matplotlib.backends.backend_pdf import PdfPages
 import os
-import warnings
+#import warnings
 import wifisQuickReduction as quickReduction
 
-os.environ['PYOPENCL_CTX'] = '1' # Used to specify which OpenCL device to target. Should be uncommented and pointed to correct device to avoid future interactive requests
+#REQUIRED INPUT
+varFile = 'wifisConfig.inp'
 
-#****************************************************************************************
-#REQUIRED INPUT FILES
-waveLstFile = 'wave.lst'
-flatLstFile = 'flat.lst'
-
-hband = False
-colorbarLims = [0,10]
-
-#****************************************************************************************
+#initialize variables using configuration file
+varInp = wifisIO.readInputVariables(varFile)
+for var in varInp:
+    globals()[var[0]]=var[1]            
 
 waveLst = wifisIO.readAsciiList(waveLstFile)
 if waveLst.ndim==0:
@@ -29,7 +25,6 @@ flatLst = wifisIO.readAsciiList(flatLstFile)
 if flatLst.ndim == 0:
     flatLst = np.asarray([flatLst])
 
-quickReduction.initPaths()
-for fle in range(len(waveLst)):
-    quickReduction.procArcData(waveLst[fle], flatLst[fle], colorbarLims=colorbarLims)
+for i in range(len(waveLst)):
+    quickReduction.procArcData(waveLst[i], flatLst[i], colorbarLims=None, hband=hband, varFile=varFile)
     
